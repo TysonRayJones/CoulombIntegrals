99b5ea85fcb349a6b04339baece7e43a57ac544d
TysonRayJones <tyson.jones.input@gmail.com>
2023-01-04 22:21:36 +0000

    test to optimise threading

--- submit.sh
+++ submit.sh
@@ -13,8 +13,51 @@
 
 module load GCCcore/10.2.0
 
-chmod +x compiler.sh
- ./compiler.sh
+#chmod +x compiler.sh
+# ./compiler.sh
  
- export OMP_NUM_THREADS=96
-./calcer 15 matrix.txt
+#export OMP_NUM_THREADS=96
+#./calcer 15 matrix.txt
+
+
+n=8
+fn=timer.txt
+tr=trash.txt
+echo 'hello\n' > $fn
+
+
+echo '\n\n1 thread:\n' >> $fn
+export OMP_NUM_THREADS=1
+time ./calcer $n $tr >> $fn
+
+echo '\n\n2 threads:\n' >> $fn
+export OMP_NUM_THREADS=2
+time ./calcer $n $tr >> $fn
+
+echo '\n\n4 threads:\n' >> $fn
+export OMP_NUM_THREADS=4
+time ./calcer $n $tr >> $fn
+
+echo '\n\n8 threads:\n' >> $fn
+export OMP_NUM_THREADS=8
+time ./calcer $n $tr >> $fn
+
+echo '\n\n16 threads:\n' >> $fn
+export OMP_NUM_THREADS=16
+time ./calcer $n $tr >> $fn
+
+echo '\n\n32 threads:\n' >> $fn
+export OMP_NUM_THREADS=32
+time ./calcer $n $tr >> $fn
+
+echo '\n\n48 threads:\n' >> $fn
+export OMP_NUM_THREADS=48
+time ./calcer $n $tr >> $fn
+
+echo '\n\n64 threads:\n' >> $fn
+export OMP_NUM_THREADS=64
+time ./calcer $n $tr >> $fn
+
+echo '\n\n96 threads:\n' >> $fn
+export OMP_NUM_THREADS=96
+time ./calcer $n $tr >> $fn
