9f2d33564a43245aa8192b1d50877bedd7340158
TysonRayJones <tyson.jones.input@gmail.com>
2023-01-04 22:28:43 +0000

    fixing timer test

--- submit.sh
+++ submit.sh
@@ -20,44 +20,8 @@ module load GCCcore/10.2.0
 #./calcer 15 matrix.txt
 
 
-n=8
-fn=timer.txt
-tr=trash.txt
-echo 'hello\n' > $fn
-
-
-echo '\n\n1 thread:\n' >> $fn
-export OMP_NUM_THREADS=1
-time ./calcer $n $tr >> $fn
-
-echo '\n\n2 threads:\n' >> $fn
-export OMP_NUM_THREADS=2
-time ./calcer $n $tr >> $fn
-
-echo '\n\n4 threads:\n' >> $fn
-export OMP_NUM_THREADS=4
-time ./calcer $n $tr >> $fn
-
-echo '\n\n8 threads:\n' >> $fn
-export OMP_NUM_THREADS=8
-time ./calcer $n $tr >> $fn
-
-echo '\n\n16 threads:\n' >> $fn
-export OMP_NUM_THREADS=16
-time ./calcer $n $tr >> $fn
-
-echo '\n\n32 threads:\n' >> $fn
-export OMP_NUM_THREADS=32
-time ./calcer $n $tr >> $fn
-
-echo '\n\n48 threads:\n' >> $fn
-export OMP_NUM_THREADS=48
-time ./calcer $n $tr >> $fn
-
-echo '\n\n64 threads:\n' >> $fn
-export OMP_NUM_THREADS=64
-time ./calcer $n $tr >> $fn
-
-echo '\n\n96 threads:\n' >> $fn
-export OMP_NUM_THREADS=96
-time ./calcer $n $tr >> $fn
+for nthreads in 1 2 4 8 16 32 48 64 96; do
+    echo '1 thread:' >> timer.txt
+    export OMP_NUM_THREADS=$nthreads
+    { time ./calcer 8 trash.txt ; } 2>> timer.txt
+done
